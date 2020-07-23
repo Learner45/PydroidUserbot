d112e19c678bf4cbcc9f54e4e5acedc95af59350
Learner45 <68682595+Learner45@users.noreply.github.com>
2020-07-23 17:41:58 +0530

    Add files via upload

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,22 +9,10 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-ALIVE_IMG = "https://telegra.ph/file/78bd74ded58f3f30a4f7c.jpg"
-ALIVE_caption = "`PYDROID USERBOT IS:` **ONLINE**\n\n"
-ALIVE_caption += "**SYSTEM STATUS**\n\n"
-ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
-ALIVE_caption += "`DATABASE STATUS:` **Functional**\n\n"
-ALIVE_caption += "**Current Branch** : `master`\n\n"
-ALIVE_caption += "**Friday OS** : `3.14`\n\n"
-ALIVE_caption += "**Current Sat** : `PydroidSat-2.25`\n\n"
-ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
-ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-ALIVE_caption += "**License** : Mit License \n\n"
-ALIVE_caption += "Copyright By [IamArjun078@Github](Github.com/IamArjun078)\n\n"
-ALIVE_caption += "[Deploy Pydroid Bot](https://github.com/IamArjun078/PydroidUserbot/)"
+ALIVE_caption = "DEVIL IS ONLINE"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.delete() 
-    await borg.send_file(alive.chat_id, ALIVE_IMG,caption=ALIVE_caption)
+    await borg.send_file(alive.chat_id, caption=ALIVE_caption)
