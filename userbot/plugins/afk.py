ba214c8f50b48f7adda3b6902a5d57932c3f60fa
Learner45 <68682595+Learner45@users.noreply.github.com>
2020-07-23 17:40:48 +0530

    Add files via upload

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -1,4 +1,4 @@
-"""AFK Plugin for Friday
+"""AFK for DEVIL
 Syntax: .afk REASON"""
 import asyncio
 import datetime
@@ -61,9 +61,9 @@ async def _(event):
             afk_time = datetime.datetime.now()  # pylint:disable=E0602
         USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
-            await event.edit(f"My Boss Is Going Afk ! And The Reason is {reason}")
+            await event.edit(f"Going Afk ! Reason: {reason}")
         else:
-            await event.edit(f"My Boss is Going")
+            await event.edit(f"Going afk")
         await asyncio.sleep(5)
         await event.delete()
         try:
@@ -121,10 +121,9 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"**My Boss is AFK** ! \n\n**Reason** : `{reason}` \n\n**Afk Since** : {afk_since}" + \
-            f"\n\n__Kindly Leave A Message__ ! \n`He Will Reply To You Soon !`" \
+        message_to_reply = f"**AFK,reason:** `{reason}` " \
             if reason \
-            else f"**Hello, Boss Is AFK Right Now And May Be Forgot List Reason ! Any Way He Will Come Back Soon !**"
+            else f"**AFK NOW.....**"
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
